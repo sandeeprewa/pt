540fa3411c94890ee6776415896fc9b79b5aa448
sandeep <91anu.sandeep@gmail.com>
2017-02-14 15:55:04 +0530

    adding db part

--- pt/src/main/java/com/pt/dl/domains/DomainDaoImpl.java
+++ pt/src/main/java/com/pt/dl/domains/DomainDaoImpl.java
@@ -1,15 +1,31 @@
 package com.pt.dl.domains;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.pt.dl.utility.SimpleHibernateTemplate;
 import com.pt.sl.domain.Domain;
 
 @Repository("domainDaoImpl")
 public class DomainDaoImpl implements DomainDao {
 
+	SimpleHibernateTemplate<Domain> simpleHibernateTemplate;
+	
+	@Autowired
+	public DomainDaoImpl(SessionFactory sessionFactory) {
+		simpleHibernateTemplate = new SimpleHibernateTemplate<Domain>(sessionFactory);
+	}
+
+	@Transactional
 	public Domain getDomain() {
-		//talk to db and return Domain
-		return null;
+		//fetching from db
+	Criteria domainCriteria = simpleHibernateTemplate.createCriteria(Domain.class);
+	domainCriteria.list();
+		return (Domain) domainCriteria.list().get(0);
 	}
 
 }
